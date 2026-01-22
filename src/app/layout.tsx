ce1dbafc8fd361eb6852b600b96c4267c7268f23
Uzayn <koleoshohuzain@gmail.com>
2026-01-22 12:38:37 +0100

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,21 @@ const outfit = Outfit({
 export const metadata: Metadata = {
   title: "My Portfolio",
   description: "Personal portfolio website",
+  icons: {
+    icon: [
+      { url: "/images/favicon.ico", sizes: "any" },
+      { url: "/images/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/images/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/images/site.webmanifest",
 };
 
 export default function RootLayout({
